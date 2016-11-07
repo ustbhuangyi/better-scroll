4d0857a52683df85dd0e281eb1311ea67428a750
ustbhuangyi <280309453@qq.com>
2016-11-07 16:54:25 +0800

    fix input bug

--- src/util/dom.js
+++ src/util/dom.js
@@ -1,57 +1,57 @@
 let elementStyle = document.createElement('div').style;
 
 let vendor = (() => {
-	let transformNames = {
-		webkit: 'webkitTransform',
-		Moz: 'MozTransform',
-		O: 'OTransform',
-		ms: 'msTransform',
-		standard: 'transform'
-	};
+  let transformNames = {
+    webkit: 'webkitTransform',
+    Moz: 'MozTransform',
+    O: 'OTransform',
+    ms: 'msTransform',
+    standard: 'transform'
+  };
 
-	for (let key in transformNames) {
-		if (elementStyle[transformNames[key]] !== undefined) {
-			return key;
-		}
-	}
+  for (let key in transformNames) {
+    if (elementStyle[transformNames[key]] !== undefined) {
+      return key;
+    }
+  }
 
-	return false;
+  return false;
 })();
 
 function prefixStyle(style) {
-	if (vendor === false) {
-		return false;
-	}
+  if (vendor === false) {
+    return false;
+  }
 
-	if (vendor === 'standard') {
-		return style;
-	}
+  if (vendor === 'standard') {
+    return style;
+  }
 
-	return vendor + style.charAt(0).toUpperCase() + style.substr(1);
+  return vendor + style.charAt(0).toUpperCase() + style.substr(1);
 }
 
 export function addEvent(el, type, fn, capture) {
-	el.addEventListener(type, fn, !!capture);
+  el.addEventListener(type, fn, !!capture);
 };
 
 export function removeEvent(el, type, fn, capture) {
-	el.removeEventListener(type, fn, !!capture);
+  el.removeEventListener(type, fn, !!capture);
 };
 
 export function offset(el) {
-	let left = 0;
-	let top = 0;
+  let left = 0;
+  let top = 0;
 
-	while (el) {
-		left -= el.offsetLeft;
-		top -= el.offsetTop;
-		el = el.offsetParent;
-	}
+  while (el) {
+    left -= el.offsetLeft;
+    top -= el.offsetTop;
+    el = el.offsetParent;
+  }
 
-	return {
-		left,
-		top
-	};
+  return {
+    left,
+    top
+  };
 };
 
 let transform = prefixStyle('transform');
@@ -62,81 +62,81 @@ export const hasTransform = transform !== false;
 export const hasTransition = prefixStyle('transition') in elementStyle;
 
 export const style = {
-	transform,
-	transitionTimingFunction: prefixStyle('transitionTimingFunction'),
-	transitionDuration: prefixStyle('transitionDuration'),
-	transitionDelay: prefixStyle('transitionDelay'),
-	transformOrigin: prefixStyle('transformOrigin'),
-	transitionEnd: prefixStyle('transitionEnd')
+  transform,
+  transitionTimingFunction: prefixStyle('transitionTimingFunction'),
+  transitionDuration: prefixStyle('transitionDuration'),
+  transitionDelay: prefixStyle('transitionDelay'),
+  transformOrigin: prefixStyle('transformOrigin'),
+  transitionEnd: prefixStyle('transitionEnd')
 };
 
 const TOUCH_EVENT = 1;
 const MOUSE_EVENT = 2;
 export const eventType = {
-	touchstart: TOUCH_EVENT,
-	touchmove: TOUCH_EVENT,
-	touchend: TOUCH_EVENT,
+  touchstart: TOUCH_EVENT,
+  touchmove: TOUCH_EVENT,
+  touchend: TOUCH_EVENT,
 
-	mousedown: MOUSE_EVENT,
-	mousemove: MOUSE_EVENT,
-	mouseup: MOUSE_EVENT
+  mousedown: MOUSE_EVENT,
+  mousemove: MOUSE_EVENT,
+  mouseup: MOUSE_EVENT
 };
 
 export function getRect(el) {
-	if (el instanceof window.SVGElement) {
-		var rect = el.getBoundingClientRect();
-		return {
-			top: rect.top,
-			left: rect.left,
-			width: rect.width,
-			height: rect.height
-		};
-	} else {
-		return {
-			top: el.offsetTop,
-			left: el.offsetLeft,
-			width: el.offsetWidth,
-			height: el.offsetHeight
-		};
-	}
+  if (el instanceof window.SVGElement) {
+    var rect = el.getBoundingClientRect();
+    return {
+      top: rect.top,
+      left: rect.left,
+      width: rect.width,
+      height: rect.height
+    };
+  } else {
+    return {
+      top: el.offsetTop,
+      left: el.offsetLeft,
+      width: el.offsetWidth,
+      height: el.offsetHeight
+    };
+  }
 };
 
 export function preventDefaultException(el, exceptions) {
-	for (let i in exceptions) {
-		if (exceptions[i].test(el)) {
-			return true;
-		}
-	}
-	return false;
+  for (let i in exceptions) {
+    if (exceptions[i].test(el[i])) {
+      return true;
+    }
+  }
+  return false;
 }
 
 export function tap(e, eventName) {
-	let ev = document.createEvent('Event');
-	ev.initEvent(eventName, true, true);
-	ev.pageX = e.pageX;
-	ev.pageY = e.pageY;
-	e.target.dispatchEvent(ev);
+  let ev = document.createEvent('Event');
+  ev.initEvent(eventName, true, true);
+  ev.pageX = e.pageX;
+  ev.pageY = e.pageY;
+  e.target.dispatchEvent(ev);
 };
 
 export function click(e) {
-	var target = e.target;
+  var target = e.target;
 
-	if (!(/(SELECT|INPUT|TEXTAREA)/i).test(target.tagName)) {
-		let ev = document.createEvent(window.MouseEvent ? 'MouseEvents' : 'Event');
-		ev.initEvent('click', true, true);
-		ev._constructed = true;
-		target.dispatchEvent(ev);
-	}
+  if (!(/(SELECT|INPUT|TEXTAREA)/i).test(target.tagName)) {
+    let ev = document.createEvent(window.MouseEvent ? 'MouseEvents' : 'Event');
+    ev.initEvent('click', true, true);
+    ev._constructed = true;
+    target.dispatchEvent(ev);
+  }
 };
 
 export function prepend(el, target) {
-	if (target.firstChild) {
-		before(el, target.firstChild);
-	} else {
-		target.appendChild(el);
-	}
+  if (target.firstChild) {
+    before(el, target.firstChild);
+  } else {
+    target.appendChild(el);
+  }
 }
 
 export function before(el, target) {
-	target.parentNode.insertBefore(el, target);
+  target.parentNode.insertBefore(el, target);
 }
