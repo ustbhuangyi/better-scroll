bd0c44a1753feef87131b87e9ce24b0df1c01814
wn7067509 <284196013@qq.com>
2017-08-19 19:01:17 +0800

    add demo: pull down refresh

---
+++ example/components/loading/loading.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="mf-loading-container">
+    <img src="./loading.gif">
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  const COMPONENT_NAME = 'loading'
+
+  export default {
+    name: COMPONENT_NAME
+  }
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+  .mf-loading-container
+    img
+      width: 20px
+      height: 20px
+      display: block
+</style>
