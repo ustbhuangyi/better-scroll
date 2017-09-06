e35a0074da6c53f7aadb3d0fcb713af1ca768a8d
AmyFoxFN <funan_amy@foxmail.com>
2017-09-06 19:24:55 +0800

    update feature

--- example/pages/features/index.vue
+++ example/pages/features/index.vue
@@ -3,15 +3,15 @@
     <ul class="feature-list">
       <li class="feature-item">
         <div class="title">体验优化</div>
-        <p>为了优化滚动体验，我们不仅提供了滚动惯性、边界回弹、滚动条等灵活配置化，还能够轻松实现下拉刷新、上拉加载等功能。</p>
+        <p>为了让滚动体验更加流畅，我们提供了惯性滚动、边界回弹、滚动条淡入淡出等效果的灵活配置，能够明显地优化 Web 滚动体验。</p>
       </li>
       <li class="feature-item">
-        <div class="title">轻量易用</div>
-        <p>经过编译后的代码大小是 46kb，压缩后是 26kb，gzip 后仅有 7kb，是一款非常轻量的 JavaScript Lib。</p>
+        <div class="title">应用丰富</div>
+        <p>其应用场景包括了滚动列表、选择器、轮播图、索引列表、开屏引导，并且能够轻松实现下拉刷新、上拉加载等功能</p>
       </li>
       <li class="feature-item">
         <div class="title">零依赖</div>
-        <p>不依赖任何框架，既可以 HTML 引用，也可以与目前前端 MVM 框架结合使用，比如官方 DEMO 就是与 Vue 的结合示例。</p>
+        <p>不依赖任何框架，既可以原生 JS 引用，也可以与任意前端 MVVM 框架结合使用，比如官方 DEMO 就是与 Vue 的结合示例。</p>
       </li>
     </ul>
   </div>
