caa1ea23d4e56a2ec7a66f8e1a8263b274c569c4
theniceangel <theniceangel@163.com>
2019-03-06 10:33:04 +0800

    wheel plugin 50%

--- src/index.ts
+++ src/index.ts
@@ -78,9 +78,9 @@ export default class BScroll extends EventEmitter {
 
     this.enable()
 
-    this.applyPlugins()
-
     this.proxy(propertiesConfig)
+
+    this.applyPlugins()
   }
 
   private applyPlugins() {
--- src/plugins/wheel/index.ts
+++ src/plugins/wheel/index.ts
@@ -1,16 +1,24 @@
 import BScroll from '../../index'
-
 export default class Wheel {
   static pluginName = 'wheel'
   wheelItemsAllDisabled: boolean
   items: HTMLCollection
   itemHeight: number
+  selectedIndex: number
+  target: EventTarget | null
   constructor(public scroll: BScroll) {
-    this.items = this.scroll.scroller.element.children
-    this.itemHeight = this.items.length ? this.scroll.scroller.scrollBehaviorY.elementSize / this.items.length : 0
     this.init()
+    this.refresh()
+    this.scroll.scroller.scrollTo(0, this.selectedIndex * this.itemHeight)
+    // refresh
     this.scroll.on(this.scroll.eventTypes.refresh, () => {
-      this._checkWheelAllDisabled()
+      this.refresh()
+    })
+    this.scroll.scroller.actionsHandler.hooks.on(this.scroll.scroller.actionsHandler.hooks.eventTypes.start, (e: TouchEvent) => {
+      this.target = e.target
+    })
+    this.scroll.scroller.hooks.on(this.scroll.scroller.hooks.eventTypes.modifyScrollMeta, (scrollMeta: any) => {
+
     })
   }
   init() {
@@ -24,8 +32,74 @@ export default class Wheel {
     if (!wheel.wheelDisabledItemClass) {
       wheel.wheelDisabledItemClass = 'wheel-disabled-item'
     }
-    if (wheel.selectedIndex === undefined) {
-      wheel.selectedIndex = 0
+  }
+
+  refresh () {
+    const wheel = this.scroll.options.wheel
+    this._checkWheelAllDisabled()
+    this.items = this.scroll.scroller.element.children
+    this.itemHeight = this.items.length ? this.scroll.scroller.scrollBehaviorY.elementSize / this.items.length : 0
+    if (this.selectedIndex === undefined) {
+      this.selectedIndex = wheel.selectedIndex || 0
+    }
+    this.scroll.maxScrollX = 0
+    this.scroll.maxScrollY = -this.itemHeight * (this.items.length - 1)
+  }
+
+  getSelectedIndex () {
+    return this.selectedIndex
+  }
+
+  wheelTo (index = 0) {
+    const y = -index * this.itemHeight
+    this.scroll.scrollTo(0, y)
+  }
+
+  private momentum(
+    current: number,
+    start: number,
+    time: number,
+    lowerMargin: number,
+    upperMargin: number,
+    wrapperSize: number,
+    options: {
+      deceleration: number
+      swipeBounceTime: number
+      swipeTime: number
+    }
+  ) {
+    let distance = current - start
+    let speed = Math.abs(distance) / time
+
+    let { deceleration, swipeBounceTime, swipeTime } = options
+    let duration = swipeTime
+    let rate = 4
+
+    let destination = current + (speed / deceleration) * (distance < 0 ? -1 : 1)
+
+    destination = this.findNearestValidWheel(destination).y
+
+    if (destination < lowerMargin) {
+      destination = wrapperSize
+        ? Math.max(
+            lowerMargin - wrapperSize / 4,
+            lowerMargin - (wrapperSize / rate) * speed
+          )
+        : lowerMargin
+      duration = swipeBounceTime
+    } else if (destination > upperMargin) {
+      destination = wrapperSize
+        ? Math.min(
+            upperMargin + wrapperSize / 4,
+            upperMargin + (wrapperSize / rate) * speed
+          )
+        : upperMargin
+      duration = swipeBounceTime
+    }
+
+    return {
+      destination: Math.round(destination),
+      duration
     }
   }
 
