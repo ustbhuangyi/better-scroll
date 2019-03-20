8ddbb80d4d9e193d15de8dee50fdb6aa2c567135
candyJ <cuijing_1031@163.com>
2019-03-20 20:40:09 +0800

    fix(zoom plugin): change hooks parent

--- src/plugins/zoom/index.ts
+++ src/plugins/zoom/index.ts
@@ -8,7 +8,6 @@ import { staticImplements, PluginCtor } from '../type'
 import { TranslaterPoint } from '../../translater'
 import Behavior from '../../scroller/Behavior'
 
-type zoomOptions = Partial<ZoomConfig> | boolean | undefined
 interface ZoomConfig {
   start: number
   min: number
@@ -46,11 +45,14 @@ export default class Zoom {
   private initScrollBoundary: ScrollBoundary
   private zooming: boolean
   private lastTransformScale: number
+  private hooksFn: Array<[EventEmitter, string, Function]>
   constructor(public scroll: BScroll) {
     this.hooks = new EventEmitter(['zoomStart', 'zoomEnd'])
     this.scroll.proxy(propertiesConfig)
     this.scroll.registerType(['zoomStart', 'zoomEnd'])
     this.zoomOpt = this.scroll.options.zoom as Partial<ZoomConfig>
+    this.lastTransformScale = this.scale
+    this.hooksFn = []
     this.init()
   }
   init() {
@@ -65,26 +67,35 @@ export default class Zoom {
       x: [scrollBehaviorX.minScrollPos, scrollBehaviorX.maxScrollPos],
       y: [scrollBehaviorY.minScrollPos, scrollBehaviorY.maxScrollPos]
     }
-    scrollerIns.hooks.on('beforeStart', (e: TouchEvent) => {
+    this.registorHooks(scrollerIns.actions.hooks, 'start', (e: TouchEvent) => {
       if (e.touches && e.touches.length > 1) {
         this.zoomStart(e)
       }
     })
-    scrollerIns.hooks.on('beforeMove', (e: TouchEvent) => {
-      if (!e.touches || e.touches.length < 2) {
-        return false
+    this.registorHooks(
+      scrollerIns.actions.hooks,
+      'beforeMove',
+      (e: TouchEvent) => {
+        if (!e.touches || e.touches.length < 2) {
+          return false
+        }
+        this.zoom(e)
+        return true
       }
-      this.zoom(e)
-      return true
-    })
-    scrollerIns.hooks.on('beforeEnd', (e: TouchEvent) => {
-      if (!this.zooming) {
-        return false
+    )
+    this.registorHooks(
+      scrollerIns.actions.hooks,
+      'beforeEnd',
+      (e: TouchEvent) => {
+        if (!this.zooming) {
+          return false
+        }
+        this.zoomEnd()
+        return true
       }
-      this.zoomEnd()
-      return true
-    })
-    scrollerIns.translater.hooks.on(
+    )
+    this.registorHooks(
+      scrollerIns.translater.hooks,
       'beforeTranslate',
       (transformStyle: string[], point: TranslaterPoint) => {
         const scale = point.scale ? point.scale : this.lastTransformScale
@@ -92,6 +103,7 @@ export default class Zoom {
         transformStyle.push(`scale(${scale})`)
       }
     )
+    this.registorHooks(scrollerIns.hooks, 'destroy', this.destroy)
   }
   zoomTo(scale: number, x: number, y: number) {
     let { left, top } = offsetToBody(this.wrapper)
@@ -101,7 +113,8 @@ export default class Zoom {
     this._zoomTo(scale, { x: originX, y: originY }, this.scale)
     this.zooming = false
   }
-  private zoomStart(e: TouchEvent) {
+
+  zoomStart(e: TouchEvent) {
     this.zooming = true
     const firstFinger = e.touches[0]
     const secondFinger = e.touches[1]
@@ -122,7 +135,7 @@ export default class Zoom {
     }
     this.scroll.trigger(this.scroll.eventTypes.zoomStart)
   }
-  private zoom(e: TouchEvent) {
+  zoom(e: TouchEvent) {
     const scrollerIns = this.scroll.scroller
     const currentDistance = this.getFingerDistance(e)
     let currentScale = (currentDistance / this.startDistance) * this.startScale
@@ -136,7 +149,7 @@ export default class Zoom {
     this.resetBoundaries(this.scale, scrollBehaviorY, 'y', y)
     scrollerIns.scrollTo(x, y, 0, undefined, {
       start: {
-        scale: this.scale / this.startScale
+        scale: this.lastTransformScale
       },
       end: {
         scale: this.scale
@@ -151,13 +164,21 @@ export default class Zoom {
 
     return getDistance(deltaX, deltaY)
   }
-  private zoomEnd() {
+  zoomEnd() {
     this._zoomTo(this.scale, this.origin, this.startScale || this.scale)
     this.zooming = false
     this.scroll.trigger(this.scroll.eventTypes.zoomEnd)
   }
+  destroy() {
+    this.hooksFn.forEach(item => {
+      const hooks = item[0]
+      const hooksName = item[1]
+      const handlerFn = item[2]
+      hooks.off(hooksName, handlerFn)
+    })
+    this.hooksFn.length = 0
+  }
   private _zoomTo(scale: number, origin: Point, startScale: number) {
-    const oldScale = this.scale
     this.scale = this.fixInScaleLimit(scale)
     const lastScale = this.scale / startScale
     const scrollerIns = this.scroll.scroller
@@ -172,7 +193,7 @@ export default class Zoom {
     if (
       scrollBehaviorX.currentPos !== Math.round(newX) ||
       scrollBehaviorY.currentPos !== Math.round(newY) ||
-      this.scale !== oldScale
+      this.scale !== this.lastTransformScale
     ) {
       scrollerIns.scrollTo(
         newX,
@@ -181,7 +202,7 @@ export default class Zoom {
         undefined,
         {
           start: {
-            scale: oldScale
+            scale: this.lastTransformScale
           },
           end: {
             scale: this.scale
@@ -232,7 +253,7 @@ export default class Zoom {
     } else if (newPos < scrollBehavior.maxScrollPos) {
       newPos = scrollBehavior.maxScrollPos
     }
-    return newPos
+    return Math.floor(newPos)
   }
 
   private scaleCure(scale: number) {
@@ -253,5 +274,9 @@ export default class Zoom {
       scale = min
     }
     return scale
+  }
+  private registorHooks(hooks: EventEmitter, name: string, handler: Function) {
+    hooks.on(name, handler, this)
+    this.hooksFn.push([hooks, name, handler])
   }
 }
