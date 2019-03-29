043356f71b78baad58cbbcb08c463f7050050209
candyJ <cuijing_1031@163.com>
2019-03-29 20:55:41 +0800

    test(animater): add test for animater

--- src/translater/__mocks__/index.ts
+++ src/translater/__mocks__/index.ts
@@ -3,7 +3,9 @@ import EventEmitter from '@/base/EventEmitter'
 const Translater = jest.fn().mockImplementation(content => {
   return {
     style: content.style,
-    hooks: new EventEmitter(['beforeTranslate', 'translate'])
+    hooks: new EventEmitter(['beforeTranslate', 'translate']),
+    getComputedPosition: jest.fn(),
+    translate: jest.fn()
   }
 })
 
