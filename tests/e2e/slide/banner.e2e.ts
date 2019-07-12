fab6956eeccefaad57ef1887d27dfb42666ecb27
CandyJ <cuijing_1031@163.com>
2019-07-12 10:42:38 +0800

    test(slide e2e): add e2e test

--- tests/e2e/slide/banner.e2e.ts
+++ tests/e2e/slide/banner.e2e.ts
@@ -51,4 +51,54 @@ describe('Slider for banner', () => {
     const currentX = curBoundingBox!.x
     await expect(currentX - oldX).toBeGreaterThan(0)
   })
+  it('should change index when drap slide', async () => {
+    await page.waitFor(300)
+    const currentIndex = await page.$eval('.docs-wrapper', el => {
+      const children = el.children
+      let index = 0
+      for (let i = 0; i < children.length; i++) {
+        if (children[i].className.indexOf('active') > -1) {
+          index = i
+          break
+        }
+      }
+      return index + 1
+    })
+    const nextDocsIndex = currentIndex === 3 ? 0 : currentIndex + 1
+    await page.dispatchSwipe(
+      [
+        [
+          {
+            x: 200,
+            y: 120
+          }
+        ],
+        [
+          {
+            x: 150,
+            y: 120
+          }
+        ],
+        [
+          {
+            x: 100,
+            y: 120
+          }
+        ],
+        [
+          {
+            x: 50,
+            y: 120
+          }
+        ]
+      ],
+      () => {},
+      30
+    )
+    const secondDots = await page.$eval(
+      `.docs-wrapper .doc:nth-child(${nextDocsIndex})`,
+      el => el.className
+    )
+    expect(secondDots).toContain('active')
+  })
 })
