ae8f20754fdbc528cea1f870526724f4e62bc23d
tank0317 <fengweiyao0317@hotmail.com>
2019-08-12 11:14:36 +0800

    chore: update indexcalculator unit test

--- packages/infinity/src/__tests__/IndexCalculator.spec.ts
+++ packages/infinity/src/__tests__/IndexCalculator.spec.ts
@@ -2,56 +2,64 @@ import IndexCalculator, { PRE_NUM, POST_NUM } from '../IndexCalculator'
 
 import FakeList from './__utils__/FakeList'
 
-import { WRAPPER_HEIGHT, DEFAULT_HEIGHT } from './__utils__/constans'
+import { WRAPPER_HEIGHT, TOMBSTONE_HEIGHT } from './__utils__/constans'
 
 describe('IndexCalculator unit test', () => {
   let indexCalculator: IndexCalculator
+  const VISIBLE_CNT = WRAPPER_HEIGHT / TOMBSTONE_HEIGHT
 
   beforeEach(() => {
-    indexCalculator = new IndexCalculator(WRAPPER_HEIGHT, DEFAULT_HEIGHT)
+    indexCalculator = new IndexCalculator(WRAPPER_HEIGHT, TOMBSTONE_HEIGHT)
   })
 
-  it('should return correct val when first visible index < PRE_NUM', () => {
+  it('should get start equal to 0 when pos is 0', () => {
     // given
-    const list = new FakeList(100).fillDom(0).getList()
-    const INDEX = 0
-    const POSITION = INDEX * DEFAULT_HEIGHT
+    const list = new FakeList(0).getList()
     // when
-    indexCalculator.calculate(POSITION - 10, list)
-    const { start, end } = indexCalculator.calculate(POSITION - 10, list)
+    const { start, end } = indexCalculator.calculate(0, list)
     // then
     expect(start).toBe(0)
+    expect(end).toBe(VISIBLE_CNT + POST_NUM)
   })
 
-  it('should return correct val when first visible index < PRE_NUM', () => {
+  it('should get start equal to 0 when first rendered index < PRE_NUM', () => {
     // given
-    const list = new FakeList(100).fillDom(0).getList()
+    const list = new FakeList(100)
+      .fillDom(0)
+      .fillPos()
+      .getList()
     const INDEX = 5
-    const POSITION = INDEX * DEFAULT_HEIGHT
+    const POSITION = INDEX * TOMBSTONE_HEIGHT
     // when
     const { start, end } = indexCalculator.calculate(POSITION, list)
     // then
     expect(start).toBe(0)
   })
 
-  it('should return correct val when first visilbe index > PRE_NUM', () => {
+  it('should get start equal to index-PRE_NUM when first visilbe index > PRE_NUM', () => {
     // given
-    const list = new FakeList(100).fillDom(0).getList()
+    const list = new FakeList(100)
+      .fillDom(0)
+      .fillPos()
+      .getList()
     const INDEX = 15
-    const POSITION = INDEX * DEFAULT_HEIGHT
+    const POSITION = INDEX * TOMBSTONE_HEIGHT
     // when
     const { start } = indexCalculator.calculate(POSITION, list)
     // then
     expect(start).toBe(INDEX - PRE_NUM)
   })
 
-  it('should return correct val when scroll up', () => {
+  it('should get correct val when scroll up and down', () => {
     // given
-    const list = new FakeList(100).fillDom(0).getList()
+    const list = new FakeList(100)
+      .fillDom(0)
+      .fillPos()
+      .getList()
     const FIRST_INDEX = 50
-    const FIRST_POSITION = FIRST_INDEX * DEFAULT_HEIGHT
+    const FIRST_POSITION = FIRST_INDEX * TOMBSTONE_HEIGHT
     const SECOND_INDEX = 40
-    const SECOND_POSITION = SECOND_INDEX * DEFAULT_HEIGHT
+    const SECOND_POSITION = SECOND_INDEX * TOMBSTONE_HEIGHT
     // when
     indexCalculator.calculate(FIRST_POSITION, list)
     const { start } = indexCalculator.calculate(SECOND_POSITION, list)
--- packages/infinity/src/__tests__/__utils__/constans.ts
+++ packages/infinity/src/__tests__/__utils__/constans.ts
@@ -1,7 +1,7 @@
-const DEFAULT_HEIGHT = 37 // 元素默认的高度
+const TOMBSTONE_HEIGHT = 37 // 元素默认的高度
 
 const WRAPPER_HEIGHT = 370 // 滚动窗口的高度
 
 const VISIBLE_CNT = 10 // 滚动窗口内能显示的元素
 
-export { DEFAULT_HEIGHT, WRAPPER_HEIGHT, VISIBLE_CNT }
+export { TOMBSTONE_HEIGHT, WRAPPER_HEIGHT, VISIBLE_CNT }
