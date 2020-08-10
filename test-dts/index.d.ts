9373f5db98dcb608bb2cc496fe7fa48fe0950e12
didi <1191340528@qq.com>
2020-08-10 19:32:17 +0800

    chore: test

---
+++ test-dts/index.d.ts
@@ -0,0 +1,6 @@
+import BetterScroll from '@better-scroll/core'
+
+export declare class BScroll extends BetterScroll {}
+
+export declare function expectType<T>(value: T): void
+export declare function expectError<T>(value: T): void
