4749ae87370c5014f7bf894ed7d1f0aeb69d192a
theniceangel <theniceangel@163.com>
2020-08-28 13:33:28 +0800

    docs(all): add beautiful icon svg

--- packages/vuepress-docs/docs/.vuepress/components/demo.vue
+++ packages/vuepress-docs/docs/.vuepress/components/demo.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="demo-wrap">
     <div class="demo-nav">
-      <i class="demo-nav-btn icon-code" @click='toggleCode'></i>
+      <i class="demo-nav-btn icon-code" @click='toggleCode'>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#666">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
+        </svg>
+      </i>
       <v-popover v-if="!hideQrcode" placement='right' :offset ='10' trigger='click'>
-        <i class="demo-nav-btn icon-qrcode"></i>
+        <i class="demo-nav-btn icon-qrcode">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#666">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v1m6 11h2m-6 0h-2v4m0-11v3m0 0h.01M12 12h4.01M16 20h4M4 12h4m12 0h.01M5 8h2a1 1 0 001-1V5a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1zm12 0h2a1 1 0 001-1V5a1 1 0 00-1-1h-2a1 1 0 00-1 1v2a1 1 0 001 1zM5 20h2a1 1 0 001-1v-2a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1z" />
+          </svg>
+        </i>
         <template slot="popover">
           <qr-code
             :url="fullQrcodeUrl"
@@ -12,6 +20,11 @@
           </qr-code>
         </template>
       </v-popover>
+      <i class="demo-nav-btn icon-preview" @click='toPreview'>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#666">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+        </svg>
+      </i>
     </div>
     <div class="demo-code" v-show="showCode">
         <div class="demo-code-nav">
@@ -27,7 +40,11 @@
             v-show="codeNavIndex === index">
             <slot :name="config.slotName"></slot>
           </div>
-          <i class="demo-code-content-copy icon-copy" @click='copyCode'></i>
+          <i class="demo-code-content-copy" @click='copyCode'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#ccc">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3" />
+            </svg>
+          </i>
           <transition name="slide-fade">
             <span class="demo-code-content-copied" v-if="copied">Copied</span>
           </transition>
@@ -82,6 +99,9 @@ export default {
     toggleCode() {
       this.showCode = !this.showCode
     },
+    toPreview() {
+      location.href = `${BASE_URL}${this.qrcodeUrl}`
+    },
     copyCode () {
       const pre = this.$el.querySelectorAll('pre')[this.codeNavIndex]
       pre.setAttribute('contenteditable', 'true')
@@ -127,12 +147,17 @@ export default {
     background-color #f5f5f5
     color rgba(0,0,0,0.87)
     .demo-nav-btn
-      margin-left 6px
+      cursor pointer
+      margin-left 8px
       font-size 22px
       font-weight bold
       align-items center
       outline 0
       border 0
+      background-size 100%
+      width 26px
+      height 26px
+      display inline-block
   .demo-code
     background-color rgb(45, 45, 45)
     border-color rgb(45, 45, 45)
@@ -169,10 +194,13 @@ export default {
       overflow-y auto
       position relative
       .demo-code-content-copy
+        cursor pointer
         position absolute
         top 30px
         right 10px
         z-index 100
+        width 26px
+        height 26px
       .demo-code-content-copied
           position absolute
           top 30px
