444182a8d2b1bc3d8f7836af3853738024b13577
theniceangel <theniceangel@163.com>
2020-10-24 12:26:14 +0800

    test(bscroll): improve coverage

--- packages/core/src/__tests__/index.spec.ts
+++ packages/core/src/__tests__/index.spec.ts
@@ -2,23 +2,42 @@ import BScroll from '../index'
 
 describe('BetterScroll Core', () => {
   let bscroll: BScroll
-  let div = document.createElement('div')
-
+  let wrapper = document.createElement('div')
+  let content = document.createElement('p')
+  wrapper.appendChild(content)
   beforeEach(() => {
-    bscroll = new BScroll(div, {})
+    bscroll = new BScroll(wrapper, {})
   })
   afterEach(() => {
     BScroll.plugins = []
     BScroll.pluginsMap = {}
   })
 
-  it('should use plugins successfully when call use()', () => {
+  it('use()', () => {
     const plugin = class MyPlugin {
       static pluginName = 'myPlugin'
     }
     BScroll.use(plugin)
+    // has installed
+    BScroll.use(plugin)
 
     expect(BScroll.plugins.length).toBe(1)
+
+    // Plugin should specify pluginName
+    const spyFn = jest.spyOn(console, 'error')
+    const unnamedPlugin = class UnnamedPlugin {}
+    BScroll.use(unnamedPlugin as any)
+    expect(spyFn).toBeCalled()
+    spyFn.mockRestore()
+
+    const spyFn2 = jest.spyOn(console, 'error')
+    // duplicate register plugins
+    const plugin2 = class MyPlugin2 {
+      static pluginName = 'myPlugin'
+    }
+    BScroll.use(plugin2)
+    expect(spyFn2).toBeCalled()
+    spyFn2.mockRestore()
   })
 
   it('should init plugins when set top-level of BScroll options', () => {
@@ -46,7 +65,59 @@ describe('BetterScroll Core', () => {
 
     expect(spy).toHaveBeenCalled()
     expect(spy).toBeCalledTimes(2)
+  })
 
-    spy.mockRestore()
+  it('disable()', () => {
+    const mockFn = jest.fn()
+    bscroll.on(bscroll.eventTypes.disable, mockFn)
+    bscroll.hooks.on(bscroll.hooks.eventTypes.disable, mockFn)
+    bscroll.disable()
+
+    expect(mockFn).toBeCalledTimes(2)
+  })
+
+  it('destroy()', () => {
+    const mockFn = jest.fn()
+    bscroll.on(bscroll.eventTypes.destroy, mockFn)
+    bscroll.hooks.on(bscroll.hooks.eventTypes.destroy, mockFn)
+    bscroll.destroy()
+
+    expect(mockFn).toBeCalledTimes(2)
+  })
+
+  it('eventRegister()', () => {
+    bscroll.eventRegister(['dummy'])
+    expect(bscroll.eventTypes.dummy).toBeTruthy()
+  })
+
+  it('should refresh when window resized', () => {
+    const mockFn = jest.fn()
+    bscroll.on(bscroll.eventTypes.refresh, mockFn)
+    bscroll.scroller.hooks.trigger(bscroll.scroller.hooks.eventTypes.resize)
+    expect(mockFn).toBeCalledTimes(1)
+  })
+
+  it('plugin wanna control scroll position ', () => {
+    const mockFn = jest.fn().mockImplementation(() => true)
+    class DummyPlugin {
+      static pluginName = 'dummy'
+      constructor(scroll: BScroll) {
+        scroll.hooks.on(scroll.hooks.eventTypes.beforeInitialScrollTo, mockFn)
+      }
+    }
+    BScroll.use(DummyPlugin)
+    bscroll = new BScroll(wrapper, { dummy: true })
+    expect(mockFn).toBeCalled()
+  })
+
+  it('should trigger contentChanged hook when content DOM has changed', () => {
+    const mockFn = jest.fn()
+    bscroll.on(bscroll.eventTypes.contentChanged, mockFn)
+
+    // content DOM has
+    wrapper.removeChild(content)
+    wrapper.appendChild(document.createElement('div'))
+    bscroll.refresh()
+    expect(mockFn).toBeCalled()
   })
 })
