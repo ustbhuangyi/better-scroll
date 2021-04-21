29a3e20333deefb6dfcbc188b042cc776fa0fb07
jizhi <theniceangel@163.com>
2021-04-21 20:57:13 +0800

    docs(docs): fix duplicated github link

--- packages/vuepress-docs/docs/.vuepress/nav/en-US.js
+++ packages/vuepress-docs/docs/.vuepress/nav/en-US.js
@@ -2,6 +2,5 @@ module.exports = [
   { text: 'Guide', link: '/en-US/guide/' },
   { text: 'Plugin', link: '/en-US/plugins/' },
   { text: 'FAQ', link: '/en-US/FAQ/' },
-  { text: 'GitHub', link: 'https://github.com/ustbhuangyi/better-scroll'},
   { text: 'Discuss', link: 'https://github.com/ustbhuangyi/better-scroll/issues'}
 ]
